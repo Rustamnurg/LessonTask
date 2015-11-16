657907b8788146b53a6197ff80a4d8daf0c90ae8
Rustam <rustik6030@gmail.com>
2015-11-16 13:02:09 +0300

    complete

---
+++ Product.java
@@ -0,0 +1,80 @@
+import java.util.GregorianCalendar;
+
+class Product{
+	private int price;
+	private GregorianCalendar bestUntil;
+	private String manufactureCountry;
+	private String name;
+	private float weight;
+
+	private boolean isBanned;
+
+	public Product(int price, GregorianCalendar bestUntil, String manufactureCountry, String name, float weight, boolean isBanned){
+		this.price = price;
+		this.bestUntil = bestUntil;
+		this.manufactureCountry = manufactureCountry;
+		this.name = name;
+		this.weight = weight;
+
+		this.isBanned = isBanned;
+	}
+
+	public boolean isFresh()
+	{
+		return bestUntil.after(GregorianCalendar.getInstance());
+	}
+
+	public boolean isBanned()
+	{
+		return this.isBanned;
+	}
+
+	public float getWeight()
+	{
+		return this.weight;
+	}
+
+	public String getMadeIn()
+	{
+		return this.manufactureCountry;
+	}
+
+	public int getPrice()
+	{
+		return this.price;
+	}
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	@Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        
+        Product p = (Product) obj;   
+        if (price != p.price)
+            return false;
+        if (bestUntil != p.bestUntil)
+            return false;
+        if (manufactureCountry != p.manufactureCountry)
+            return false;
+        if (name != p.name)
+            return false;
+        if (weight != p.weight)
+            return false;
+        if (isBanned != p.isBanned)
+            return false;
+            
+        return true;
+    }
+
+    @Override
+    public String toString()
+    {
+    	return "Product name: " + name + " price: " + Integer.toString(price) + 
+    		" made in " + manufactureCountry + " weight " + Float.toString(weight);
+    }
+}
